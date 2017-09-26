5f4db14eebce9f5bf1ccb76d6d2105fdce641829
Frank Sedlar <frank.sedlar@gza.com>
2017-09-26 18:47:08 -0400

    Final Files

--- stock_db.py
+++ stock_db.py
@@ -1,14 +1,17 @@
 
 import psycopg2
 import sys
+from urllib2 import urlopen
+import shutil
 
 from psycopg2.extensions import AsIs
-#current_table = 'stock_values_3'
 
 
 #import scraper function
 from stock_scraper import scraper 
 
+
+## Set up a postgres table
 #connect to the postres db
 conn_string = "host='localhost' dbname='stocks' user='frank' password='B00tl3gged'"
 
@@ -17,19 +20,24 @@ print "Opened databse successfully"
 
 #create a table in the postres db
 cur = conn.cursor()
-cur.execute('CREATE TABLE stock_values_6(ID serial PRIMARY KEY, Name VARCHAR(40), Price VARCHAR(40), Opening VARCHAR(40), Volume VARCHAR(40), DT VARCHAR(40))')
+cur.execute('CREATE TABLE stock_values(ID serial PRIMARY KEY, Name VARCHAR(40), Price VARCHAR(40), Opening VARCHAR(40), Volume VARCHAR(40), DT VARCHAR(40))')
 
 
 print "Table created successfully"
 
-stock_info = scraper()
 
-SQL = '''INSERT INTO stock_values_6 (ID, Name, Price, Opening, Volume, DT) VALUES (%s, %s, %s, %s, %s, %s)'''
-data = (1, stock_info[0], stock_info[1], stock_info[2], stock_info[3], stock_info[4])
+## Insert data for each stock into the table
+urls = ['https://www.bloomberg.com/quote/MSFT:US', 'https://www.bloomberg.com/quote/WMT:US', 'https://www.bloomberg.com/quote/VZ:US', 'https://www.bloomberg.com/quote/ACM:US', 'https://www.bloomberg.com/quote/TWTR:US','https://www.bloomberg.com/quote/TWLO:US']
 
-cur.execute(SQL, data)
+for url in urls:
+	stock_info = scraper(url)
 
-#cur.execute('INSERT INTO stock_values_6(ID, Name, Price, Opening, Volume, DT) VALUES (1, stock_stuff[0], stock_stuff[1], stock_stuff[2], stock_stuff[3], stock_stuff[4])')
+	SQL = '''INSERT INTO stock_values (Name, Price, Opening, Volume, DT) VALUES (%s, %s, %s, %s, %s)'''
+	data = (stock_info[0], stock_info[1], stock_info[2], stock_info[3], stock_info[4])
+
+	cur.execute(SQL, data)
+
+
 
 conn.commit()
 print "Records created successfully"
